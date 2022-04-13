029cc491a3c94053405b18616294295900b0c20c
Tiity <anthony.cadet@ocito.com>
2022-04-13 17:02:45 +0200

    Todo correction TP

--- src/App.java
+++ src/App.java
@@ -1,9 +1,9 @@
-import pojo.Soldier;
+import pojo.*;
 
 public class App {
     public static void main(String[] args) {
         // Je créer mon héro
-        Soldier hero = new Soldier("Xavier", 200, 5, 5);
+        AbstractCharacter hero = new Soldier("Xavier", 200, 5, 5);
         Soldier enemy = new Soldier("Christophe", 100, 10, 8);
 
         // Je le fait se déplacer
--- src/pojo/ICharacter.java
+++ src/pojo/ICharacter.java
@@ -2,4 +2,5 @@ package pojo;
 
 public interface ICharacter {
     void speak(String replique);
+    // voir si on peut remonter des methods de Solider ou Abstract Character
 }
